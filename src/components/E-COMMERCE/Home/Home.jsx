6c35cb1136d390c67b927764574b9a6ae436e4f8
tkm02 <mamadoutuo77@gmail.com>
2023-11-21 15:44:01 +0000

    first-commit

---
+++ src/components/E-COMMERCE/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+class Home extends PureComponent {
+    state = {
+            
+        }
+
+    render() {
+        return (
+            <h1>Hello world</h1>
+        )
+    }
+}
+
+export default Home
