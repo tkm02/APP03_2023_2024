689aac01cc9fa95700dc2fac8dab7e9571ac0009
tkm02 <mamadoutuo77@gmail.com>
2023-11-24 21:39:51 +0000

    routage-02

--- src/components/MesRoutes.js
+++ src/components/MesRoutes.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import '../styles/MesRoutes/MesRoutes.css'
 import { Link } from 'react-router-dom';
 const MesRoutes = () => {
     return (
-        <div>
-            <Link to="/ecom">ECOM</Link>
-            <Link to="/admin/login">ADMIN</Link>
-            <Link to="/cooperative">COOPERATIVE</Link>
-            <Link to="/producteur">PRODUCTEUR</Link>
+        <div className='container-routes'>
+            <h1>LES LIENS VERS LES DEFFERENTES PAGES</h1>
+            <Link className='routes' to="/ecom">ECOM</Link>
+            <Link className='routes' to="/admin/login">ADMIN</Link>
+            <Link className='routes' to="/cooperative">COOPERATIVE</Link>
+            <Link className='routes' to="/producteur">PRODUCTEUR</Link>
         </div>
     );
 }
